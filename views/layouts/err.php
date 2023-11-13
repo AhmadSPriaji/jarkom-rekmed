d0ddd9680c4870f318faa1443249dcfd192240f0
davinjason <davinjasonevanraharjo@mail.ugm.ac.id>
2023-11-13 11:12:59 +0700

    format document

--- views/layouts/err.php
+++ views/layouts/err.php
@@ -3,29 +3,29 @@
 <!-- BEGIN HEAD -->
 
 <head>
-    <meta charset="utf-8" />
-    <title>Rekam Medis | Error</title>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta content="width=device-width, initial-scale=1" name="viewport" />
-    <meta content="Preview page of <?= Yii::getAlias('@web/') ?>metronic Admin Theme #1 for 404 page option 2" name="description" />
-    <meta content="" name="author" />
-    <!-- BEGIN GLOBAL MANDATORY STYLES -->
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
-    <link href="<?= Yii::getAlias('@web/') ?>metronic/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <link href="<?= Yii::getAlias('@web/') ?>metronic/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
-    <link href="<?= Yii::getAlias('@web/') ?>metronic/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="<?= Yii::getAlias('@web/') ?>metronic/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
-    <!-- END GLOBAL MANDATORY STYLES -->
-    <!-- BEGIN THEME GLOBAL STYLES -->
-    <link href="<?= Yii::getAlias('@web/') ?>metronic/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
-    <link href="<?= Yii::getAlias('@web/') ?>metronic/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
-    <!-- END THEME GLOBAL STYLES -->
-    <!-- BEGIN PAGE LEVEL STYLES -->
-    <link href="<?= Yii::getAlias('@web/') ?>metronic/pages/css/error.min.css" rel="stylesheet" type="text/css" />
-    <!-- END PAGE LEVEL STYLES -->
-    <!-- BEGIN THEME LAYOUT STYLES -->
-    <!-- END THEME LAYOUT STYLES -->
-    <link rel="shortcut icon" href="favicon.ico" />
+	<meta charset="utf-8" />
+	<title>Rekam Medis | Error</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta content="width=device-width, initial-scale=1" name="viewport" />
+	<meta content="Preview page of <?= Yii::getAlias('@web/') ?>metronic Admin Theme #1 for 404 page option 2" name="description" />
+	<meta content="" name="author" />
+	<!-- BEGIN GLOBAL MANDATORY STYLES -->
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+	<link href="<?= Yii::getAlias('@web/') ?>metronic/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?= Yii::getAlias('@web/') ?>metronic/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?= Yii::getAlias('@web/') ?>metronic/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="<?= Yii::getAlias('@web/') ?>metronic/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+	<!-- END GLOBAL MANDATORY STYLES -->
+	<!-- BEGIN THEME GLOBAL STYLES -->
+	<link href="<?= Yii::getAlias('@web/') ?>metronic/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
+	<link href="<?= Yii::getAlias('@web/') ?>metronic/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
+	<!-- END THEME GLOBAL STYLES -->
+	<!-- BEGIN PAGE LEVEL STYLES -->
+	<link href="<?= Yii::getAlias('@web/') ?>metronic/pages/css/error.min.css" rel="stylesheet" type="text/css" />
+	<!-- END PAGE LEVEL STYLES -->
+	<!-- BEGIN THEME LAYOUT STYLES -->
+	<!-- END THEME LAYOUT STYLES -->
+	<link rel="shortcut icon" href="favicon.ico" />
 </head>
 <!-- END HEAD -->
 
